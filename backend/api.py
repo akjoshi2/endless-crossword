4b360c2cbd301a53e02a70e4d9c904a2fecaa298
Elon <elonmusk@gmail.com>
2023-11-04 17:48:13 -0500

    api'

--- backend/api.py
+++ backend/api.py
@@ -2,12 +2,19 @@ import sqlite3
 import requests
 import csv
 import json
-from flask import Flask
+from flask import Flask, request
+import os
+import subprocess
+import random
 app = Flask(__name__)
 
-@app.route("/getCross")
+@app.route("/getCross", methods=["GET"])
 def getCross():
-    pass
+    diff = request.args["difficulty"]
+    l = [i for i in os.path.walk(f"crosswordsolver/Structures/{diff}")]
+    p = subprocess.Popen(["./crosswordsolver", "wordlist.txt", random.choice(l) ], stdout=subprocess.PIPE)
+    stdout = p.communicate()
+    #parse
 
 
 
