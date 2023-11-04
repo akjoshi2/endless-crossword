d2eae7282ce9c9dcfbbc0d62df8b323feeee6802
Sriram Bharadwaj <sriram@Srirams-MacBook-Pro.local>
2023-11-04 14:37:34 -0500

    finished settings?

--- frontend/src/components/CrosswordSettings.jsx
+++ frontend/src/components/CrosswordSettings.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const CrosswordSettings = (props) => {
-    const [value, setValue] = React.useState('');
+    const [seed, setSeed] = useState('');
+    const [difficulty, setDifficulty] = useState('');
 
-    const queryAPI = (seed, ) => {
-
-    }
-    const handleChange = (event) => {
-        setValue(event.target.value);
+    const handleSeed = (event) => {
+        setSeed(event.target.value);
     };
 
-    const handleDifficulty = () => {
-        
-    }
+    const handleDifficulty = (event) => {
+        setDifficulty(event.target.value);
+    };
+
+    const queryAPI = () => {
+        // Implement your API query logic here
+        // You can use 'seed' and 'difficulty' in your query
+        // TODO: FIX FAKEURL
+        let fakeurl = ""
+        fetch(fakeurl)
+        .then((response) => response.json())
+        .then((result) => {
+            // Handle the API response
+            console.log(result);
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+    };
+
     return (
         <Card>
             <CardContent>
-                {/* Our dropdown options for difficulty */}
+                {/* Dropdown options for difficulty */}
                 <FormControl>
                     <InputLabel htmlFor="difficulty">Select Difficulty</InputLabel>
                     <Select
-                    value={difficulty}
-                    onChange={handleDifficulty}
-                    input={<Input id="difficulty" />}
+                        value={difficulty}
+                        onChange={handleDifficulty}
+                        input={<TextField id="difficulty" variant="outlined" />}
                     >
                         <MenuItem value="easy">Easy</MenuItem>
                         <MenuItem value="medium">Medium</MenuItem>
@@ -35,21 +54,23 @@ const CrosswordSettings = (props) => {
                     </Select>
                 </FormControl>
 
-                {/* Our seed*/}
+                {/* Seed input */}
                 <form>
                     <TextField
                         label="Enter seed:"
                         variant="outlined"
-                        value={value}
-                        onChange={handleChange}
+                        value={seed}
+                        onChange={handleSeed}
                     />
                 </form>
 
-                {/* Our button */}
-                <Button variant="contained" color="primary" onClick="queryAPI">Generate</Button>
-            
+                {/* Generate button */}
+                <Button variant="contained" color="primary" onClick={queryAPI}>
+                    Generate
+                </Button>
             </CardContent>
         </Card>
-      );
-}
-export default CrosswordSettings
+    );
+};
+
+export default CrosswordSettings;
