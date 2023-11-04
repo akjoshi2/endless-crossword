8f9924892d49f7e7fa150d1d3893d175862823aa
Elon <elonmusk@gmail.com>
2023-11-04 18:36:10 -0500

    changes

--- backend/db.py
+++ backend/db.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sqlite3
 
 df = pd.read_parquet("0000.parquet")
+df.answer = df.answer.apply(lambda x: x.upper().replace(" ", ""))
 df = df[df['clue'].notnull() & df["answer"].notnull()]
 df = df[df["clue"].str.len() < 2000]
 con = sqlite3.connect("crossword")
