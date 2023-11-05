ba0a30b9ce5f314ae6f1722f5e4cb99d7f18c38d
akjoshi2 <akjoshi2003@gmail.com>
2023-11-05 00:08:21 -0500

    Remove Card and use Grid for CrosswordSettings

--- fe2/src/CrosswordSettings.jsx
+++ fe2/src/CrosswordSettings.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Card, CardContent, Button, TextField, FormControl, InputLabel, Select, MenuItem, Input, createTheme,
-    ThemeProvider, Typography } from "@mui/material";
+    ThemeProvider, Typography, Grid } from "@mui/material";
 import {Container, Row, Col} from 'react-bootstrap';
 import eventBus from "./eventBus";
 export const CrosswordSettings = () => {
@@ -80,9 +80,58 @@ export const CrosswordSettings = () => {
     return (
         <ThemeProvider theme={selectedTheme}>
             <Container fluid={true}>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                    <FormControl style={{ marginBottom: '20px', width: '100%', marginTop: '20px'}}>
+                            <InputLabel style={{minWidth: 100, maxWidth: 1000 }} htmlFor="difficulty-label">Select Difficulty</InputLabel>
+                            <Select
+                                label="Select Difficulty"
+                                value={difficulty}
+                                onChange={handleDifficulty}
+                                input={<Input id="difficulty" />}
+                            >
+                                <MenuItem value="Easy">Easy</MenuItem>
+                                <MenuItem value="Medium">Medium</MenuItem>
+                                <MenuItem value="Hard">Hard</MenuItem>
+                            </Select>
+                    </FormControl>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <form style={{ margin: "20px 0 0 0", marginBottom: '20px' }}>
+                            <TextField
+                                label="Current Seed:"
+                                variant="outlined"
+                                disabled
+                                value={currSeed}
+                            />
+                    </form>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <form style={{ margin: "20px 0 0 0", marginBottom: '20px' }}>
+                            <TextField
+                                label="Custom Seed:"
+                                variant="outlined"
+                                value={customSeed}
+                                onChange={handleSeed}
+                            />
+                    </form>
+                    </Grid>
+                    <Grid item xs={3}>
+                    <Button style={{margin: "30px 0 0 0", background: "#a7d8ff", color:"#000000"}}variant="contained" color="primary" onClick={queryAPI}>
+                            Generate
+                    </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </ThemeProvider>
+
+    );
+
+    /*return (
+        <ThemeProvider theme={selectedTheme}>
+            <Container fluid={true}>
                 <Card style={{ width: "70%", margin: "20px 27%", display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '10px' }}>
                     <CardContent>
-                        {/* Dropdown options for difficulty */}
                         <FormControl style={{ marginBottom: '20px', width: '100%', marginTop: '20px'}}>
                             <InputLabel style={{minWidth: 100, maxWidth: 1000 }} htmlFor="difficulty-label">Select Difficulty</InputLabel>
                             <Select
@@ -94,12 +143,10 @@ export const CrosswordSettings = () => {
                                 <MenuItem value="Easy">Easy</MenuItem>
                                 <MenuItem value="Medium">Medium</MenuItem>
                                 <MenuItem value="Hard">Hard</MenuItem>
-                                {/* Add more MenuItem components as needed */}
                             </Select>
                         </FormControl>
                     </CardContent>
                     <CardContent>
-                        {/* Current seed disabled input */}
                         <form style={{ margin: "20px 0 0 0", marginBottom: '20px' }}>
                             <TextField
                                 label="Current Seed:"
@@ -110,7 +157,6 @@ export const CrosswordSettings = () => {
                         </form>
                     </CardContent>
                     <CardContent>
-                        {/* Custom seed input */}
                         <form style={{ margin: "20px 0 0 0", marginBottom: '20px' }}>
                             <TextField
                                 label="Custom Seed:"
@@ -121,14 +167,12 @@ export const CrosswordSettings = () => {
                         </form>
                     </CardContent>
                     <CardContent>
-                        {/* Generate button */}
                         <Button style={{margin: "30px 0 0 0", background: "#a7d8ff", color:"#000000"}}variant="contained" color="primary" onClick={queryAPI}>
                             Generate
                         </Button>
                     </CardContent>
-                    
                 </Card>
             </Container>
         </ThemeProvider>
-    );
+    );*/
 };
