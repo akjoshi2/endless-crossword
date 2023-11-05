c5306ab511c0634741a427085eb1922b9163614f
akjoshi2 <akjoshi2003@gmail.com>
2023-11-04 21:22:32 -0500

    Event bus to convey change

--- fe2/src/CrosswordSettings.jsx
+++ fe2/src/CrosswordSettings.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
 import { Card, CardContent, Button, TextField, FormControl, InputLabel, Select, MenuItem, Input, createTheme,
-    ThemeProvider, } from "@mui/material";
+    ThemeProvider, Typography } from "@mui/material";
 import {Container, Row, Col} from 'react-bootstrap';
+import eventBus from "./eventBus";
 export const CrosswordSettings = () => {
-    const [seed, setSeed] = useState(Math.floor(Math.random() * (2000000000 + 1)));
-    const [difficulty, setDifficulty] = useState('');
+    const [currSeed, setCurrSeed] = useState(4);
+    const [customSeed, setCustomSeed] = useState(0);
+    const [difficulty, setDifficulty] = useState('Hard');
     const [isDarkMode, setIsDarkMode] = useState(false);
 
     const handleSeed = (event) => {
-        setSeed(event.target.value);
+        setCustomSeed(event.target.value);
     };
 
     const handleDifficulty = (event) => {
@@ -38,17 +40,13 @@ export const CrosswordSettings = () => {
 
     const queryAPI = () => {
         
-
-            fetch(`https://cheesehacks.onrender.com/getCross?difficulty=${difficulty}&seed=${seed}`)
+            setCurrSeed(customSeed);
+            fetch(`https://cheesehacks.onrender.com/getCross?difficulty=${difficulty}&seed=${customSeed}`)
             .then((response) => response.json())
             .then((result) => {
                 // Handle the API response
                 console.log(result);
-                currElement = document.createElement("div");
-                currElement.id = ""
-                currElement.innerText = response.json();
-                currElement.style.display = "none";
-                document.insertBefore(currElement, document.body.firstChild);
+                eventBus.emit('reactDOMChange',result);
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
@@ -58,7 +56,7 @@ export const CrosswordSettings = () => {
     return (
         <ThemeProvider theme={selectedTheme}>
             <Container fluid={true}>
-                <Card style={{ width: "70%", margin: "20px 27%", display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '10px' }}>
+                <Card style={{ width: "70%", margin: "20px 27%", display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '10px' }}>
                     <CardContent>
                         {/* Dropdown options for difficulty */}
                         <FormControl style={{ marginBottom: '20px', width: '100%'}}>
@@ -79,10 +77,17 @@ export const CrosswordSettings = () => {
                     <CardContent>
                         {/* Seed input */}
                         <form style={{ margin: "20px 0 0 0", marginBottom: '20px' }}>
+                            <Typography variant="subtitle1">Current Seed:</Typography>
+                            <Typography variant="body1">{currSeed}</Typography>
+                        </form>
+                    </CardContent>
+                    <CardContent>
+                        {/* Seed input */}
+                        <form style={{ margin: "20px 0 0 0", marginBottom: '20px' }}>
                             <TextField
-                                label="Enter seed:"
+                                label="Custom Seed:"
                                 variant="outlined"
-                                value={seed}
+                                value={customSeed}
                                 onChange={handleSeed}
                             />
                         </form>
